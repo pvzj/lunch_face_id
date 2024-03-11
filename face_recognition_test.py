d7215537ac8c6c0e0f0816d1e6039d825fd8f9fe
Jeremy Chen <jchen@eastsideprep.org>
2024-03-11 14:44:31 -0700

    comments and cleaned up some style

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -1,34 +1,40 @@
+# imports
 import face_recognition as fr
 import cv2
 import numpy as np
 import glob
 
-video_capture = cv2.VideoCapture(0)
+# webcam input number (0 is default builtin webcam)
+WEBCAM_INPUT_NUMBER = 0
 
+# number of frames the person needs to be in front of the camera
+# for the program to actually make a request
+VERIFICATION_FRAMES = 10
+
+# take video capture from source
+video_capture = cv2.VideoCapture(WEBCAM_INPUT_NUMBER)
+
+# known face encodings, face names to be stored
 known_face_encodings = []
 known_face_names = []
 
-
+# iterate through all pictures in the folder
 for filename in glob.glob('pics/*.jpg'):
     print(filename)
+    # load image
     im = fr.load_image_file(filename)
-    name = filename.removeprefix('pics\\').removesuffix('.jpg')
 
+    # create encoding of the face
     encoding = fr.face_encodings(im)[0]
 
+    # get person's name be removing prefix, suffix
+    person_name = filename.removeprefix('pics\\').removesuffix('.jpg')
+
+    # add encoding, name to list
     known_face_encodings.append(encoding)
-    known_face_names.append(name)
+    known_face_names.append(person_name)
 
-
-# jeremy_image = 
-# jeremy_encoding = fr.face_encodings(jeremy_image)[0]
-
-# vedant_image = fr.load_image_file('vedant.jpg')
-# vedant_encoding = fr.face_encodings(vedant_image)[0]
-
-
-
-# Initialize some variables
+# initialize variables
 face_locations = []
 face_encodings = []
 face_names = []
@@ -38,6 +44,10 @@ prev_name = ''
 
 frame_count = 0
 
+# code taken from 
+# https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_from_webcam_faster.py
+
+# iterate indefinitely
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -48,31 +58,31 @@ while True:
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        # rgb_small_frame = small_frame[:, :, ::-1]
+        # make it a contiguous array
         rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+
         # Find all the faces and face encodings in the current frame of video
         face_locations = fr.face_locations(rgb_small_frame)
         face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
 
+        # get list for face names
         face_names = []
+        # iterate through all encodings
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = fr.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
+            # use the known face with the smallest distance to the new face
             face_distances = fr.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
+            # add to face names
             face_names.append(name)
 
+    # toggle process boolean
     process_this_frame = not process_this_frame
 
 
@@ -82,7 +92,7 @@ while True:
     found_face = False
     max_top, max_right, max_bottom, max_left = 0, 0, 0, 0
 
-    # Display the results
+    # iterate through face locations, face names
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
@@ -90,37 +100,55 @@ while True:
         bottom *= 4
         left *= 4
 
+        # find area of face
         height = abs(top - bottom)
         width = abs(right-left)
-
         area = height*width
-        # print(area, " ", max_area)
+
+        # if current area > max_area, set the maximum
+        # use this to find the closest face out of all the potential faces
         if area > max_area:
             max_area = area
             max_top, max_right, max_bottom, max_left = top, right, bottom, left
             max_name = name
             found_face = True
-    # print (max_area)
+    
+    # if a face has been found, process it
+    # otherwise, reset the frame count
     if found_face:
+        # set the color of the box being drawn
         color = ()
-        # Draw a box around the face
+        
+        # if the name is unknown, make it red
+        # otherwise, make it proportional to the frame count
         if max_name == 'Unknown':
             color = (0, 0, 255)
         else:
-            color = (0, frame_count * 25, 250-frame_count*25)
+            color = (0, frame_count * (255/VERIFICATION_FRAMES), 250-frame_count*(255/VERIFICATION_FRAMES))
+
+        # draw rectangle to encompass face
         cv2.rectangle(frame, (max_left, max_top), (max_right, max_bottom), color, 2)
 
         # Draw a label with a name below the face
         cv2.rectangle(frame, (max_left, max_bottom - 35), (max_right, max_bottom), color, cv2.FILLED)
+        # put text
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, max_name, (max_left + 6, max_bottom - 6), font, 1.0, (255, 255, 255), 1)
 
+        # if name is not unknown, process the picture
+        # otherwise, continue
         if max_name != 'Unknown':
+            # check if the current name is the same as the name of the previous frame
+            # if so, process
+            # if not, reset hte counter
             if max_name == prev_name:
-                cv2.putText(frame, (str(min(frame_count*10, 100)) +'%'), (max_left + 6, max_bottom - 60), font, 1.0, (255, 255, 255), 1)
+                # put percentage data on screen
+                cv2.putText(frame, (str(min(frame_count*(round(100/VERIFICATION_FRAMES, 2)), 100)) +'%'), (max_left + 6, max_bottom - 60), font, 1.0, (255, 255, 255), 1)
                 
+                # increment frame count
                 frame_count+=1
-                if frame_count == 10:
+                # if person is present for the number of verification frames, create order request
+                if frame_count == VERIFICATION_FRAMES:
                     print('order from: ', max_name)
             else:
                 prev_name = max_name   
@@ -129,22 +157,6 @@ while True:
             prev_name = 'Unknown'    
     else:
         frame_count = 0
-
-#  # Draw a box around the face
-#     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-#     # Draw a label with a name below the face
-#     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-#     font = cv2.FONT_HERSHEY_DUPLEX
-#     cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-#     if name != 'Unknown' and name != prev_name:
-#         print('order from: ', name)
-#         prev_name = name   
-
-        
-
-    
 
     # Display the resulting image
     cv2.imshow('Video', frame)
